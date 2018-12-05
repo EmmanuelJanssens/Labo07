0e1414644deefb4240dc1f755c48af1df07fa51e
Emmanuel Janssens <emmanuel.janssens@heig-vd.ch>
2018-12-05 10:24:38 +0100

    Prototypes
    First protottypes

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+    Robin
+*/
+string convertArabToRoman(string value);
+/*
+    Robin
+*/
+string convertDigitToRoman(char digit,int index);
+
+char getRomanChar(int number);
+
+
+/*
+    Rochana/emmanuel
+*/
+string checkConversionType(string entry);
+/*
+    Rochana/Emmanuel
+*/
+string convertRomanToArab(string value);
+
+
+
+/*
+    Rochana/emmanuel
+*/
+int getArabNumber(char roman);
+
+
+int main()
+{    
+    return 0;
+}
